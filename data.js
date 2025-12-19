c7d30bd2a5db7572960878e6be2cba1fb96b8ee6
razel01 <eastfino0819@gmail.com>
2025-12-19 22:29:21 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,18 @@
 const games = [
+   {
+    id: 2,
+    title: "Winter Memories",
+    image: "wintermemories.jpg",
+    description: "Winter Memories adalah sekuel langsung dari Summer Memories yang mengambil latar waktu saat liburan musim dingin. Pemain kembali berperan sebagai protagonis yang mengunjungi rumah bibi dan sepupunya di pedesaan yang kini tertutup salju. Game simulasi kehidupan dewasa ini menawarkan peningkatan besar dari pendahulunya, seperti peta yang lebih luas, karakter baru, serta fitur "Dual Heroine" yang memungkinkan interaksi intim dengan dua karakter sekaligus. Selain aktivitas harian seperti memancing es dan mengelola statistik, fokus utamanya tetap pada pengembangan hubungan romantis yang lebih mendalam dan konten eksplisit yang lebih variatif, semuanya disajikan dengan gaya pixel-art yang sangat mendetail.",
+    download: "https://1024terabox.com/s/1-ZYuFJ_CK-P8symSWTknVw"
+  }
   {
     id: 1,
     title: "Summer Memories",
     image: "summermemories.jpg",
-    description: "Summer Memories adalah game simulasi kehidupan dewasa bergaya pixel-art tentang seorang pemuda yang menghabiskan liburan musim panas di rumah bibinya di pedesaan. Fokus utamanya adalah manajemen waktu harian untuk melakukan berbagai aktivitas seperti memancing atau menangkap serangga sambil membangun hubungan romantis yang intim dengan karakter wanita di sekitar desa. Game ini sangat populer karena kualitas animasinya yang halus dan nuansa nostalgia liburan yang kental, namun ditujukan khusus untuk audiens dewasa karena mengandung konten eksplisit.",
+    description: "Summer Memories adalah game simulashttps://github.com/Razel4444/nicegame/edit/main/data.js#L1C15i kehidupan dewasa bergaya pixel-art tentang seorang pemuda yang menghabiskan liburan musim panas di rumah bibinya di pedesaan. Fokus utamanya adalah manajemen waktu harian untuk melakukan berbagai aktivitas seperti memancing atau menangkap serangga sambil membangun hubungan romantis yang intim dengan karakter wanita di sekitar desa. Game ini sangat populer karena kualitas animasinya yang halus dan nuansa nostalgia liburan yang kental, namun ditujukan khusus untuk audiens dewasa karena mengandung konten eksplisit.",
     download: "https://1024terabox.com/s/1-ZYuFJ_CK-P8symSWTknVw"
   }
   
 ];
+
