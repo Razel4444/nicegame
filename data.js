5ecfd8e690898f86f551253cc8a1e30c269565a2
razel01 <eastfino0819@gmail.com>
2025-12-19 22:22:12 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+const games = [
+  {
+    id: 1,
+    title: "Summer Memories",
+    image: "summermemories.jpg",
+    description: "Summer Memories adalah game simulasi kehidupan dewasa bergaya pixel-art tentang seorang pemuda yang menghabiskan liburan musim panas di rumah bibinya di pedesaan. Fokus utamanya adalah manajemen waktu harian untuk melakukan berbagai aktivitas seperti memancing atau menangkap serangga sambil membangun hubungan romantis yang intim dengan karakter wanita di sekitar desa. Game ini sangat populer karena kualitas animasinya yang halus dan nuansa nostalgia liburan yang kental, namun ditujukan khusus untuk audiens dewasa karena mengandung konten eksplisit.",
+    download: "https://1024terabox.com/s/1-ZYuFJ_CK-P8symSWTknVw"
+  }
+  
+];
